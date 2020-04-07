b00bb7d99e2d040bcf232cd225c3801fdb4be84a
Nicola Tonellotto <nicola.tonellotto@gmail.com>
2020-04-07 10:21:40 +0200

    code cleanup

--- exercises/wordcount/src/main/java/it/unipi/hadoop/WordCount.java
+++ exercises/wordcount/src/main/java/it/unipi/hadoop/WordCount.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
